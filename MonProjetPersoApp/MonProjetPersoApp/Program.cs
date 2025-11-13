e4ef54b331c21ff899d0048ff0f56f5d6ba3e8de
Aymen3423 <aymen.modica10@gmail.com>
2025-11-13 10:20:08 +0100

    Ajout demande age + verif si il est majeur

--- MonProjetPersoApp/MonProjetPersoApp/Program.cs
+++ MonProjetPersoApp/MonProjetPersoApp/Program.cs
@@ -4,3 +4,12 @@ if (name == null)
 {
     Console.WriteLine("invalid");
 }
+
+Console.WriteLine("Ta quel age ?");
+string responceAge = Console.ReadLine(); int age = Convert.ToInt32(responceAge);
+
+if(age < 18)
+{
+    Console.WriteLine("Tu n'est pas majeur quitte le programe en appuyant sur un touche ");
+    Console.ReadLine();
+}
